8f3bd7eb088c1f391f9d1eda643e4c421e385274
javedharoon7 <javedharoon7@gmail.com>
2020-05-03 23:36:02 -0700

    televisions added

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 var Faker = require('./lib');
 var faker = new Faker({ locales: require('./lib/locales') });
 module['exports'] = faker;
-console.log(faker.television.brand());
-console.log(faker.television.platform());
-console.log(faker.television.price());
+console.log(faker.televisions.brands());
+console.log(faker.televisions.platforms());
+console.log(faker.televisions.prices());
